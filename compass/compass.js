2566a7196ed498e2fd6f45276f1cfef7ee88899e
kimio_kosaka <kim@banana.local>
2018-04-25 08:33:20 +0900

    bs

--- compass/compass.js
+++ compass/compass.js
@@ -10,30 +10,6 @@ const MAGNETOMETER_BEARING_UUID = 'e95d9715-251d-470a-a062-fa1922dfa9a8'
 // コンパス読取りインターバル mS
 const INTERVAL = 250
 
-/*
-function connect () {
-  navigator.bluetooth.requestDevice({
-    filters: [{
-      namePrefix: 'BBC micro:bit'
-    }],
-    optionalServices: [MAGNETOMETER_SERVICE_UUID]
-  })
-    .then(device => {
-      magnetometerDevice = device
-      console.log('device', device)
-      return device.gatt.connect()
-    })
-    .then(server => {
-      console.log('server', server)
-      findMagnetometerService(server)
-    })
-    .catch(error => {
-      alert('BLE接続に失敗しました。もう一度試してみてください')
-      console.log(error)
-    })
-}
-*/
-
 function connect () {
   navigator.bluetooth.requestDevice({
     filters: [{
@@ -67,21 +43,14 @@ function disconnect () {
   document.js.x.value = ''
 }
 
-function findMagnetometerService (server) {
-  server.getPrimaryService(MAGNETOMETER_SERVICE_UUID)
-    .then(service => {
-      findMagnetometerPeriodCharacteristic(service)
-      findMagnetometerBearingCharacteristic(service)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-}
-
 function findMagnetometerPeriodCharacteristic (service) {
   service.getCharacteristic(MAGNETOMETER_PERIOD_UUID)
     .then(characteristic => {
-      writeMagnetometerPeriodValue(characteristic)
+      // writeMagnetometerPeriodValue(characteristic)
+      characteristic.writeValue(new Uint16Array([INTERVAL]))
+        .catch(error => {
+          console.log(error)
+        })
     })
     .catch(error => {
       console.log(error)
@@ -98,7 +67,14 @@ function writeMagnetometerPeriodValue (characteristic) {
 function findMagnetometerBearingCharacteristic (service) {
   service.getCharacteristic(MAGNETOMETER_BEARING_UUID)
     .then(characteristic => {
-      startMagnetometerBearingNotification(characteristic)
+      // startMagnetometerBearingNotification(characteristic)
+      characteristic.startNotifications()
+        .then(char => {
+          console.log('Compass:', char)
+          alert('BLE接続が完了しました。')
+          characteristic.addEventListener('characteristicvaluechanged',
+            onMagnetometerBearingChanged)
+        })
     })
     .catch(error => {
       console.log(error)
