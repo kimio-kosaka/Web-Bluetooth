3589251c584e59067591c53601f213751d937d59
kimio_kosaka <kim.kosmac@gmail.com>
2018-04-25 14:00:54 +0900

    Update accelerometer.js

--- accelerometer/accelerometer.js
+++ accelerometer/accelerometer.js
@@ -4,7 +4,6 @@ const DEVICE_NAME_PREFIX = 'BBC micro:bit'
 // micro:bit BLE Accelerometer UUID
 const     ACCELEROMETERSERVICE_SERVICE_UUID = 'e95d0753-251d-470a-a062-fa1922dfa9a8'
 const ACCELEROMETERDATA_CHARACTERISTIC_UUID = 'e95dca4b-251d-470a-a062-fa1922dfa9a8'
-
 // Messages
 const MSG_CONNECTED = 'BLE接続が完了しました。'
 const MSG_CONNECT_ERROR = 'BLE接続に失敗しました。もう一度試してみてください'
